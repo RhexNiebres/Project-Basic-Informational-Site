b8b93c30e4bd1fdc19d7b4f8691f6e0b979d21f7
RhexNiebres <rhexniebres@gmail.com>
2025-02-18 16:13:45 -0800

    fix spacing

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ http.createServer(function (req, res) {
   if (!filename.endsWith('.html')) {
     filename += '.html';
   }
-  
+
   fs.readFile(filename, function(err, data) {
     if (err) {
-
         fs.readFile('./404.html', function(err404, data404){
             res.writeHead(404, {'Content-Type': 'text/html'});
             res.write(err404 ? '404 Not Found' : data404);
       return res.end();
         });
-    } else{
+
+    } else{s
         res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
